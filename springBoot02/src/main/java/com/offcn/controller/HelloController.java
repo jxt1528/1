d52592e7d3f2051eda2c24d952ec1013958ba32c
jxt <jin1282914923@163.com>
2020-09-05 18:13:44 +0800

    添加了一个controller

---
+++ springBoot02/src/main/java/com/offcn/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.offcn.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+    public static void main(String[] args) {
+
+    }
+}
